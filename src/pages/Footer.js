83934c5ecca4444da6e7c2a9c2b291e596364410
Francis Ong <francis.ong25@gmail.com>
2022-04-25 00:06:44 +0800

    add icons fontawesome react

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,27 +1,37 @@
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+  FaEnvelope,
+  FaArrowUp,
+} from 'react-icons/fa';
+
 const Footer = () => {
   return (
     <>
       <footer class="container-fluid bg-dark text-white footer-text" id="contact">
         <h4 class="text-center text-white pt-1 pt-md-4 mb-0">
             <a target="_blank" href="https://www.facebook.com/FrancisCondinoOng" class="home-link me-sm-4 me-2" rel="noreferrer">
-                <i class="fab fa-facebook-f"></i>
+              <FaFacebookF />
             </a>
             <a target="_blank" href="https://twitter.com/imfranz25" class="home-link me-sm-4 me-2" rel="noreferrer">
-                <i class="fab fa-twitter"></i>
+              <FaTwitter />
             </a>
             <a target="_blank" href="https://www.instagram.com/franzzz_25/" class="home-link me-sm-4 me-2" rel="noreferrer">
-                <i class="fab fa-instagram"></i>
+              <FaInstagram />
             </a>
             <a target="_blank" href="https://github.com/imfranz25" class="home-link me-sm-4 me-2" rel="noreferrer">
-                <i class="fab fa-github"></i>
+              <FaGithub />
             </a>
             <a target="_blank" href="https://www.linkedin.com/in/francis-ong25/" class="home-link me-sm-4 me-2" rel="noreferrer">
-                <i class="fab fa-linkedin"></i>
+              <FaLinkedin />
             </a>
         </h4>
         <p class="d-flex align-items-center justify-content-center p-0 m-0 pb-2 pb-lg-0">
             Contact :
-            <a target="_blank" href="mailto:francis.ong25@gmail.com" class="text-nowrap home-link mx-1" rel="noreferrer"><i class="fa fa-envelope"></i></a>
+            <a target="_blank" href="mailto:francis.ong25@gmail.com" class="text-nowrap home-link mx-1" rel="noreferrer"><FaEnvelope /></a>
             francis.ong25@gmail.com
         </p>
         <div class="row">
@@ -30,12 +40,14 @@ const Footer = () => {
             <p class="p-0 m-0">&copy; 2022 Francis Ong &bull; All Rights Reserved.</p>
           </div>
           <div class="col-12 d-flex justify-content-center align-items-center mb-3">
-            <p class="p-0 m-0">Made with custom CSS, JS and  Bootstrap 5.</p>
+            <p class="p-0 m-0">Made on the top of React, Bootstrap, and custom CSS & JS.</p>
           </div>
         </div>
       </footer>
       <button class="m-5 bg-warning border-0 p-0 rounded d-none" id="nav-home">
-        <a href="#home" class="px-3" rel="noreferrer"><i class="fa fa-arrow-up text-light py-2 my-1"></i></a>
+        <a href="#home" class="px-3" rel="noreferrer">
+          <i><FaArrowUp /></i>
+        </a>
       </button>
     </>
   )
