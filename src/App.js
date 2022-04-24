43139143b7e67f6e4e870d1904046873cd3f9e98
Francis Ong <francis.ong.pixel8@gmail.com>
2022-04-24 09:48:26 +0800

    reset files to original state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import './css/Main.scss';
-import {Routes, Route} from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Home from './pages/HomePage';
-import About from './pages/AboutPage';
-import Events from './pages/EventsPage';
-import Contact from './pages/ContactPage';
-//import {Home, About, Events, Contact} from './pages';
-
+//import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <NavBar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/events' element={<Events />} />
-        <Route path='/contact' element={<Contact />} />
-      </Routes>
+      <h1>Francis Ong</h1>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 //styles
 import 'bootstrap/dist/css/bootstrap.css';
-//import $ from 'jquery';
-//import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './index.css';
+import './css/index.css';
 
 
 ReactDOM.render(
