0318995508a4f450fc16c880a4e69e5641f8970f
Francis Ong <francis.ong25@gmail.com>
2022-04-28 20:52:43 +0800

    add new content to sidebar

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,13 @@ const HomePage = () => (
     <p>In this blog, I have included a whole load of blog examples from a wide variety of different niches, all run on different blogging platforms like WordPress, Joomla! and Drupal</p>
     <p>Each example listed in this blog post are all different in some way and all bring something unique to their readers & subscribers. I want to show you what is possible and how you can take inspiration from them and create an awesome blog of your own.</p>
     <p>Some of these blogs make over $100k a month, others are just a hobby for their owners, but all have the same purpose at their core… the love of writing and sharing information.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, suscipit placeat ea perspiciatis laborum, necessitatibus ducimus est temporibus quidem dolorem fuga. Aut sint quis odit ut? Inventore natus excepturi corporis?
+    Nesciunt fuga obcaecati minima accusamus excepturi numquam minus odit doloremque , perferendis inventore ullam nihil.
+
+    Mollitia perferendis unde repudiandae doloribus delectus suscipit eaque corrupti? Non, architecto reiciendis debitis earum animi placeat quas, sit commodi nesciunt cumque perferendis dolorem laboriosam repellat ratione ea quia officia error?
+    Inventore voluptas illum blanditiis iure temporibus vitae accusamus illo hic maiores, consequuntur recusandae laborum explicabo mollitia error possimus expedita nostrum facere quae quo. Tenetur, vel consectetur. Adipisci quo iste laudantium?</p>
+    <p>Mollitia perferendis unde repudiandae doloribus delectus suscipit eaque corrupti? Non, architecto reiciendis debitis earum animi placeat quas, sit commodi nesciunt cumque perferendis dolorem laboriosam repellat ratione ea quia officia error?
+    Inventore voluptas illum blanditiis iure temporibus vitae accusamus illo hic maiores, consequuntur recusandae laborum explicabo mollitia error possimus expedita nostrum facere quae quo. Tenetur, vel consectetur. Adipisci quo iste laudantium?</p>
   </div>
 );
 
--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,13 +1,18 @@
 const Sidebar = () => (
-  <aside className='sidebar-container container'>
+  <aside className='sidebar-container container mx-3'>
     <div className='card p-4'>
       <h4 className='text-center'>Over 1 Million Monthly Readers</h4>
-      <h6 className='text-center my-3'>Follow Us</h6>
+      <h6 className='text-center my-5'>Follow Us</h6>
       <h6 className='text-center'>
         <a href='mailto:francis.ong25@gmail.com' className='text-success email-me'>Join our email list here</a>
         <div>Get fresh content straight to your inbox</div>
         <div>from francis the great :)</div>
       </h6>
+    </div>
+    <div className='card p-r'>
+      <div className='img-container'>
+        <img alt="Start a Blog" src="https://makeawebsitehub.com/wp-content/uploads/2018/03/how-to-start-a-blog.jpg" />
+      </div>
     </div>
   </aside>
 )
